ba174ee3ba6da5a0220d0c18718219327722205d
SuhasTest <suhas@kerv.com>
2025-01-27 12:48:32 +0530

    commit cehcking pr

--- src/test/java/stepDefination/RegisteredLoginScenario.java
+++ src/test/java/stepDefination/RegisteredLoginScenario.java
@@ -7,12 +7,12 @@ public class RegisteredLoginScenario
 	@Given("user registered in the application")
 	public void user_registered_in_the_application() 
 	{
-	   dfsdf
+	   
 	}
 
 	@Then("they should be able to login with same credentials")
 	public void they_should_be_able_to_login_with_same_credentials() 
 	{
-	  sdfsdf
+	  
 	}
 }
